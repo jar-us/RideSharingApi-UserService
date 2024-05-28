35fd6936bfcf3737376c580a3114b8e69c44858a
jar-us <suraj.sharma3963@gmail.com>
2024-05-28 14:43:23 +0530

    first commit

---
+++ src/main/kotlin/jar/us/userservice/RideSharingApiUserServiceApplication.kt
@@ -0,0 +1,11 @@
+package jar.us.userservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RideSharingApiUserServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<RideSharingApiUserServiceApplication>(*args)
+}
