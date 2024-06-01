b46de9b5e0985b56653f55333c7968d92cd023fe
jar-us <suraj.sharma3963@gmail.com>
2024-06-01 18:38:23 +0530

    Converted to multi module project structure

--- app/src/main/kotlin/jar/us/userservice/RideSharingApiUserServiceApplication.kt
+++ app/src/main/kotlin/jar/us/userservice/RideSharingApiUserServiceApplication.kt
@@ -1,11 +1,14 @@
 package jar.us.userservice
 
+import jar.us.printHello
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class RideSharingApiUserServiceApplication
 
-fun main(args: Array<String>) {
-    runApplication<RideSharingApiUserServiceApplication>(*args)
+fun main() {
+    runApplication<RideSharingApiUserServiceApplication>()
+    println("Hello, World!!!")
+    printHello()
 }
---
+++ specifications/src/main/kotlin/CotDo.kt
@@ -0,0 +1,5 @@
+package jar.us
+
+fun printHello() {
+    println("Hello from CotDo!")
+}
